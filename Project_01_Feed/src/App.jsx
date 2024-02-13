dcb36d9cbe6c0125e94760ecc3c838fd0cdcbdbd
Afra Taíza <afra.taiza@rdstation.com>
2024-02-13 02:33:31 -0300

    feat: add new components in the App

--- Project_01_Feed/src/App.jsx
+++ Project_01_Feed/src/App.jsx
@@ -1,9 +1,17 @@
+import { Header } from "./components/Header/Header";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
-  
+
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <Header />
+      <main className={styles.wrapper}>
+        <Sidebar />
+        <section>Feed</section>
+      </main>
     </>
   )
 }
