dc1d2829535eb8b1b487d58e5d426f373c63aa28
Afra Taíza <afra.taiza@rdstation.com>
2024-02-18 16:15:00 -0300

    feat: add api fetch

--- Project_01_Feed/src/App.jsx
+++ Project_01_Feed/src/App.jsx
@@ -3,8 +3,35 @@ import { Header } from "./components/Header";
 import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch('https://my-json-server.typicode.com/afrataiza/posts_mock_api/posts');
+      const data = await response.json();
+      setPosts(data);
+    }
+    getPosts();
+
+    const getComments = async () => {
+      const response = await fetch('https://my-json-server.typicode.com/afrataiza/posts_mock_api/comments');
+      const data = await response.json();
+      setComments(data);
+    };
+    getComments();
+
+    const getAuthors = async () => {
+      const response = await fetch('https://my-json-server.typicode.com/afrataiza/posts_mock_api/authors');
+      const data = await response.json();
+      setAuthors(data);
+    };
+    getAuthors();
+  }, []);
 
   return (
     <>
@@ -12,7 +39,10 @@ function App() {
       <main className={styles.wrapper}>
         <Sidebar />
         <section>
-          <Post />
+          {
+
+          }
+          <Post posts={posts} comments={comments} authors={authors} />
         </section>
       </main>
     </>
