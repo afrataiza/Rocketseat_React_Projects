87492f2b9375fd42abceffb24fee87839a6eb021
Afra Taíza <afra.taiza@rdstation.com>
2024-02-25 22:12:42 -0300

    feat: update component, add props e functionality

--- Desafio_01_ToDo_List/src/components/Task.tsx
+++ Desafio_01_ToDo_List/src/components/Task.tsx
@@ -1,15 +1,40 @@
 import { Trash } from 'phosphor-react';
 import styles from './Task.module.css';
 
-export function Task() {
+interface TaskProps {
+  title: string;
+  isCompleted: boolean;
+  id: number;
+  handleToggleTask: (id: number) => void;
+  createdAt: string;
+  handleRemoveTask: (id: number) => void;
+}
+
+export function Task({
+  title,
+  isCompleted,
+  id,
+  handleToggleTask,
+  createdAt,
+  handleRemoveTask,
+}: TaskProps) {
   return (
-    <div className={styles.task}>
-      <input
-        type="checkbox"
-        id="task"
-      />
-      <label htmlFor="task">Tarefa 1</label>
-      <Trash size={24} />
-    </div>
+    <form className={styles.task}>
+      <div>
+        <input
+          type="checkbox"
+          id="task"
+          checked={isCompleted}
+          onChange={() => handleToggleTask(id)}
+        />
+        <label htmlFor="task">{title}</label>
+        <button
+          type="button"
+          onClick={() => handleRemoveTask(id)}>
+          <Trash size={24} />
+        </button>
+      </div>
+      <span>{createdAt}</span>
+    </form>
   );
 }
